d96e09f64abb4940482b884ea0fa8e3e9ea5a8d2
kip_parcell <kipluis@gmail.com>
2021-03-28 22:15:02 -0700

    fixed a few things in Powers controller

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -30,18 +30,6 @@ class PowersController < ApplicationController
         end
     end
 
-    def show
-        if params[:artist_id]
-          @artist = Artist.find_by(id: params[:artist_id])
-          @song = @artist.songs.find_by(id: params[:id])
-          if @song.nil?
-            redirect_to artist_songs_path(@artist), alert: "Song not found"
-          end
-        else
-          @song = Song.find(params[:id])
-        end
-      end
-
 
 
     def create
